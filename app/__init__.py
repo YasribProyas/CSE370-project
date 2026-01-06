af0548162d8712daaab594f9a7624996eefe62e4
Yasrib Proyas <abyashrirproyas@gmail.com>
2026-01-07 01:18:20 +0600

    fixed shaj problems

--- app/__init__.py
+++ app/__init__.py
@@ -1,23 +1,32 @@
 from flask import Flask
+from flask_bootstrap import Bootstrap5
 from dotenv import load_dotenv
 import os
 
 load_dotenv()
 
+bootstrap = Bootstrap5()
+
 def create_app():
     app = Flask(__name__)
     app.config['SECRET_KEY'] = os.getenv('SECRET_KEY', '1234lalala')
-
+    
+    bootstrap.init_app(app)
+    
     from app.auth import auth_bp
     from app.main import main_bp
-    from app.proyas import proyas_bp
-    from app.shaj import shaj_bp
-    from app.fahim import fahim_bp
 
     app.register_blueprint(auth_bp)
     app.register_blueprint(main_bp)
+
+
+
+    from app.proyas import proyas_bp
+    from app.shaj import shaj_bp
+    from app.fahim import fahim_bp
+    
     app.register_blueprint(proyas_bp)
     app.register_blueprint(shaj_bp)
     app.register_blueprint(fahim_bp)
-
+    
     return app
